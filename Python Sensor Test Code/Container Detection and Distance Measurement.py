0465b69c6b46df0380373d6befba264241b07b83
Ethan Crawford <ethan_jc@hotmail.com>
2021-08-23 11:42:00 +0800

    Begin attempting to play sounds
    This change works towards being able to play sounds when something crosses the
    boundaries of the 'detection zone'.
    
    TODO:
    - 'real-time' sensor input with debouncing/smoothing
    - throttling of the sound triggering

---
+++ Python Sensor Test Code/Container Detection and Distance Measurement.py
@@ -0,0 +1,66 @@
+#  Ref: http://www.d3noob.org/2018/04/this-post-is-part-of-book-raspberry-pi.html
+
+
+#  !/usr/bin/python
+#  encoding:utf-8
+
+import RPi.GPIO as GPIO                    #  Import GPIO library
+import time  #  Import time library
+import subprocess # Used for creating a separate process for playing a sound
+from gpiozero import LineSensor
+from signal import pause
+GPIO.setwarnings(False)
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)                     #  Set GPIO pin numbering
+
+TRIG = 18                                  #  Associate pin 15 to TRIG
+ECHO = 24                                  #  Associate pin 14 to Echo
+
+ContSensor = LineSensor(21)# Opto output
+ContSensor.when_line = lambda: print('Waiting for container')
+ContSensor.when_no_line = lambda: print('Container detected')
+
+print("Distance measurement in progress")
+
+GPIO.setup(TRIG, GPIO.OUT)                  #  Set pin as GPIO out
+GPIO.setup(ECHO, GPIO.IN)                   #  Set pin as GPIO in
+
+current_distance = -1.0
+previous_distance = -1.0
+INNER_BOUND = 20
+OUTER_BOUND = 400
+
+while True:
+    GPIO.output(TRIG, False)                 #  Set TRIG as LOW
+    print("Waiting For Sensor To Settle")
+    time.sleep(5)                            #  Delay of 5 seconds
+
+    GPIO.output(TRIG, True)                  #  Set TRIG as HIGH
+    time.sleep(0.00001)                      #  Delay of 0.00001 seconds
+    GPIO.output(TRIG, False)                 #  Set TRIG as LOW
+
+    while GPIO.input(ECHO) == 0:               #  Check if Echo is LOW
+        pulse_start = time.time()              #  Time of the last  LOW pulse
+
+    while GPIO.input(ECHO) == 1:               #  Check whether Echo is HIGH
+        pulse_end = time.time()                #  Time of the last HIGH pulse
+
+        pulse_duration = pulse_end - pulse_start  #  pulse duration to a variable
+
+    current_distance = pulse_duration * 17150        #Calculate distance
+    current_distance = round(current_distance, 2)    #Round to two decimal points
+
+    # If the most recent pair of distances show a pattern of moving from outside to inside the 'detection zone'
+    if((current_distance > INNER_BOUND and current_distance < OUTER_BOUND) and
+       (previous_distance <= INNER_BOUND or previous_distance >= OUTER_BOUND)):
+        # play a sound in a separate process
+        subprocess.Popen(['aplay', '~/Desktop/sound.wav'])
+    # If the most recent pair of distances show a pattern of moving from inside to outside the 'detection zone'
+    elif((current_distance <= INNER_BOUND or current_distance >= OUTER_BOUND) and
+         (previous_distance > INNER_BOUND and previous_distance < OUTER_BOUND)):
+        # play a sound in a separate process
+        subprocess.Popen(['aplay', '~/Desktop/sound.wav'])
+    else:
+        print("Out Of Range")                   #display out of range
+
+    previous_distance = current_distance
